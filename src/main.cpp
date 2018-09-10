892af2caded63e412ed2fa4dab4c97ae0eab4fdb
Bruno Dupuy <bruno.dupuy@cern.ch>
2018-09-10 17:05:39 +0200

    Clean and change blinking info.

--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,51 @@
 #include <Preferences.h>
 #include <time.h>
 
+/*
+ Embedded LED facilities
+ Continus = AccessPoint waitting a wifi host selection
+ blinking = Wait Station connection is OK
+*/
 #define EspLedBlue 2
+/*
+ Button BOOT on ESP32 is pressed more than 3 seconds
+ reset all wifi_host selection
+*/
 #define ButtonBoot 0
 
 #define optAP2Host 0
 #define optStaOK   1
 #define optHostRST 2
 
+// Debug facilities
 #define DBX_EVT false
+#define DBX_MSG true
+
+#define MAX_STA_WAIT 40  // One loop take 0.5 sec
 
 // Access point Initialize
-// IPAddress Ip(192, 168, 1, 4);
-// IPAddress NMask(255, 255, 255, 0);
 const char* ipAP                    = "192.168.4.1";
+const IPAddress                     apIP(192, 168, 4, 1);
 const char* ssid                    = "ESP32-Dudu";
 const char* password                = "123456789";
 const char* ntpServer               = "pool.ntp.org";
 const long  gmtOffset_sec           = 3600;
 const int   daylightOffset_sec      = 3600;
-static volatile bool wifi_connected = false;
+static volatile bool wifi_stationok = false;
+static volatile bool wifi_accessPts = false;
 Preferences preferences;       // The NVM storage
 String headerIn;               // Html Header Input
 String htmlSsidList;           // Html selector Wifi list
 String wifiSSID;               // The SSID use
 String wifiPAWD;               // Accosiate password
-int flashLed = 0;              // Led flashing 3 time = IP is correct
+int flashLed = 0;              // current loop into MAX_STA_WAIT
 uint32_t previousMillis = 0;   // Last milli-second
 uint8_t seconds = 0;           // Seconds [0-59]
-uint8_t closeAPinSec = 0;      // Close AP
 uint8_t incBtBoot = 0;         // ButtonBoot pressed during time in seconds
 WiFiServer server(80);         // Set web server port number to 80
 struct tm timeinfo;            // time struct
 
+// Decode specific char into url (Space become a +, ...)
 String urlDecode(const String& text) {
 	String decoded = "";
 	char temp[] = "0x00";
@@ -49,23 +62,21 @@ String urlDecode(const String& text) {
 			decodedChar = strtol(temp, NULL, 16);
 		}	else {
 			if (encodedChar == '+')	 decodedChar = ' ';
-			else  decodedChar = encodedChar; // normal ascii char
+			else  decodedChar = encodedChar;
 		}
 		decoded += decodedChar;
 	}
 	return decoded;
 }
 
-String getMacAddress() {
-	return String(WiFi.macAddress().c_str());
-}
-
+// Time HH:MM.ss
 String getTime() {
 	char temp[10];
 	sprintf(temp, "%02d:%02d:%02d", timeinfo.tm_hour,timeinfo.tm_min,timeinfo.tm_sec );
 	return String(temp);
 }
 
+// Date as europeen format
 String getDate(){
 	char temp[20];
 	sprintf(temp, "%02d/%02d/%04d %02d:%02d:%02d",
@@ -73,6 +84,7 @@ String getDate(){
 	return String(temp);
 }
 
+// Just some event are used see wifi.h enum
 void WiFiEvent(WiFiEvent_t event) {
 	switch (event) {
 		case SYSTEM_EVENT_WIFI_READY:
@@ -106,10 +118,7 @@ void WiFiEvent(WiFiEvent_t event) {
 		break;
 		case SYSTEM_EVENT_STA_GOT_IP:
 		if (DBX_EVT) Serial.println(">> SYSTEM_EVENT_STA_GOT_IP");
-		{
-			flashLed = 3; // 3 flash indicat that we have an IP
-			configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //init and get the time
-		}
+		configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //init and get the time
 		break;
 		case SYSTEM_EVENT_STA_DISCONNECTED:
 		if (DBX_EVT) Serial.println(">> SYSTEM_EVENT_STA_DISCONNECTED");
@@ -118,11 +127,12 @@ void WiFiEvent(WiFiEvent_t event) {
 		if (DBX_EVT) Serial.println(">> SYSTEM_EVENT_SCAN_DONE");
 		break;
 		default:
-		if (DBX_EVT) Serial.print(">> default:"); Serial.println(event, DEC);
+		if (DBX_EVT) { Serial.print(">> default:"); Serial.println(event, DEC); }
 		break;
 	}
 }
 
+// Scanning SSID available
 void wifiList () {
 	WiFi.mode(WIFI_STA);
 	WiFi.disconnect();
@@ -140,17 +150,21 @@ void wifiList () {
 	}
 }
 
-void  setAccessPoint() {       // Connect to Wi-Fi network with SSID and password
-	Serial.println();
+ // Connect to Wi-Fi network with SSID and password
+void  setAccessPoint() {
+	if (DBX_EVT) Serial.println();
+	wifi_accessPts = true;
+	wifi_stationok = false;
 	digitalWrite(EspLedBlue, HIGH);
 	wifiList();                  // Read list of SSID
 	WiFi.softAP(ssid, password); // Remove the password parameter, if you want the AP (Access Point) to be open
 	delay (100 );
-	// WiFi.softAPConfig(Ip, Ip, NMask);
 	IPAddress IP = WiFi.softAPIP();
-	Serial.print("Select WiFi Access Point: '"); Serial.print(ssid);
-	Serial.print("' and open your browser on: ");
-	Serial.print("http://");  Serial.println(IP);
+	if (DBX_MSG) {
+		Serial.print("Select WiFi Access Point: '"); Serial.print(ssid);
+	  Serial.print("' and open your browser on: ");
+	  Serial.print("http://");  Serial.println(IP);
+	}
 	server.begin();
 }
 
@@ -165,7 +179,6 @@ void htmlHeader(WiFiClient client) {
 }
 
 void htmlNotConnected(WiFiClient client) {
-	// Display the HTML web page
 	htmlHeader(client);
 	client.println("<style>");
 	client.println("html{font-family:Helvetica;display:inline-block;margin:0 auto;text-align:center;background-color:#eee;}");
@@ -191,7 +204,7 @@ void htmlNotConnected(WiFiClient client) {
 	client.println("</div>");
 	client.println("</form></body></html>");
 	client.println(); // The HTTP response ends with another blank line
-	Serial.println("End htmlNotConnected HTML");
+	if (DBX_MSG) Serial.println("End htmlNotConnected HTML");
 }
 
 void htmlConnectedOk(WiFiClient client, int opt) {
@@ -210,20 +223,15 @@ void htmlConnectedOk(WiFiClient client, int opt) {
 		client.println(WiFi.localIP());
 		client.println("</b> NTP Time:"); client.println(getDate());
 	}
-	//  client.println(); // The HTTP response ends with another blank line
-	//if (opt==optStaOK) {
+
 	client.println("<br><hr><a href=\"/reset\"><button class=\"button\">Change WiFi</button></a>");
-	//}
-	//if (opt==optHostRST || opt==optStaOK) {
 	client.println("<br><hr>Select WiFi Access Point: <b>");
 	client.println( ssid);
 	client.println("</b> and open your browser on IP:<b> ");
 	client.println(ipAP);
-	//}
+
 	client.println("</body></html>");
-	//	IPAddress IP = WiFi.localIP();
-	//	Serial.println(">>>> Local IP: "); Serial.print(IP);Serial.println(" ");
-	//	client.stop();
+  if (DBX_MSG)  Serial.println("End htmlConnectedOk HTML");
 }
 
 void putPreferences(String ssid, String pwd) {
@@ -236,132 +244,170 @@ void putPreferences(String ssid, String pwd) {
 
 void connectToHost() {
 	WiFi.disconnect();
-	wifi_connected = false;
-	Serial.print("Try to connect WIFI: '"); Serial.print(wifiSSID);Serial.print("' PWD :"); Serial.println(wifiPAWD);
-	int mxl=0;
+	wifi_stationok = false;
+	if (DBX_MSG)  { Serial.print("Try to connect WIFI: '"); Serial.print(wifiSSID);Serial.print("' PWD :"); Serial.println("wifiPAWD"); }
+	flashLed=0;
 	WiFi.begin(wifiSSID.c_str(), wifiPAWD.c_str());
 	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-		if (mxl++ > 40) {
-			Serial.println(".");
+		delay(250),
+		digitalWrite(EspLedBlue, HIGH);
+		delay(250);
+		digitalWrite(EspLedBlue, LOW);
+		if (DBX_MSG) Serial.print(".");
+		if (flashLed++ > MAX_STA_WAIT) {
+			if (DBX_MSG) Serial.println(".");
 			setAccessPoint(); // Start accesPoint because wifiSSID host is not availabale
 			return;
 		}
 	}
+	flashLed = 0;
 	// Store new Wifi ssid and password
 	putPreferences(wifiSSID, wifiPAWD);
 	digitalWrite(EspLedBlue, LOW);
-	Serial.println("");
-	Serial.print("WiFi connected to [Host][IP]:[");Serial.print(wifiSSID);
-	Serial.print("][]");Serial.print(WiFi.localIP());Serial.println("]");
+	if (DBX_MSG) {
+	  Serial.println("");
+	  Serial.print("WiFi connected to [Host][IP]:[");Serial.print(wifiSSID);
+	  Serial.print("][]");Serial.print(WiFi.localIP());Serial.println("]");
+  }
 	server.begin();
-	closeAPinSec = 3; // 1 == AP stop()
-	wifi_connected = true;
+	wifi_stationok = true;
 }
 
 void fromWiFiAP (WiFiClient client) {
-	if (headerIn.indexOf("POST /ap HTTP/") >= 0) { // wifiSSID is selected Try  connection
+
+	// wifiSSID is selected Try  connection
+	if (headerIn.indexOf("POST /ap HTTP/") >= 0) {
 		int sw = headerIn.indexOf("wifi=");
 		int e = headerIn.indexOf("&password=");
 		wifiSSID = urlDecode(headerIn.substring(sw+5, e));
 		wifiPAWD = urlDecode(headerIn.substring(e+10));
 		htmlConnectedOk(client, optAP2Host);      // Answer connection OK
-		delay(1000);
+		delay(1000);                              // Delay to sent page
 		connectToHost();                          // Store new Wifi ssid, password and connect
 	} else {
 		htmlNotConnected(client);
 	}
 }
 
-void fromWiFiSTA(WiFiClient client) {         // Loop when Esp is connected to Wifi
+// Loop while Esp is connected to Wifi host
+void fromWiFiSTA(WiFiClient client) {
 	if (headerIn.indexOf("GET /reset") >= 0) {  // Back to AP for wifi selection
 		putPreferences("none","none");
 		htmlConnectedOk(client, optHostRST);
-		Serial.println("REBOOT SENDING BY HTTP");
 		client.stop();
 		delay(500);
 		ESP.restart();
 	} else {
-		htmlConnectedOk(client, optStaOK);
+		htmlConnectedOk(client, optStaOK);  // Normal page
 	}
 }
 
 void loopWifi() {
 	WiFiClient client = server.available();   // listen for incoming clients
 	if (client) {                             // if you get a client,
-		Serial.println("New client");
+		if (DBX_MSG) Serial.println("New client");
 		if (client.connected()) {
 			unsigned long timeout = millis();
 			while ( client.available() == 0 ) {  // wait a correct message
 				if (millis() - timeout > 5000) {
-					Serial.println("Timeout client");
+					if (DBX_MSG) Serial.println("Timeout client");
 					client.stop();
 					return;
 				}
 			}
-			while ( client.available() > 0 ) {    // if there's bytes to read from the client,
-			char c = client.read();
-			headerIn += c;
-		}
-		if (headerIn.length() > 0 ) {
-			Serial.println(headerIn);
-			if (wifi_connected == false)
-			fromWiFiAP(client);           // AccessPoint html fom for wifi selection
-			else
-			fromWiFiSTA(client);          // Wifi Host selection is OK
-			headerIn = "";                  // Clear the headerIn variable
-		}
-	}
-	Serial.println("End client");
-}
+			while ( client.available() > 0 ) {
+			  headerIn += client.read();      // Concate char into headerIn
+		  }
+		  if (headerIn.length() > 0 ) {
+			  if (DBX_MSG) Serial.println(headerIn);
+			  if (wifi_stationok == false)
+			    fromWiFiAP(client);           // AccessPoint html fom for wifi selection
+			  else
+			    fromWiFiSTA(client);          // Wifi Host selection is OK
+			  headerIn = "";                  // Clear the headerIn variable
+		  }
+	  }
+	  if (DBX_MSG) Serial.println("End client");
+  }
 }
 
 void setup() {
-	delay(5000);
 	Serial.begin(115200);
+
+	// Wait a little bit for serial connection
+	delay(5000);
+
+	// Set MAC addess & AP IP
 	byte new_mac[8] = {0x30,0xAE,0xA4,0x90,0xFD,0xC8}; // Pif
 	//  byte new_mac[8] = {0x8C,0x29,0x37,0x43,0xCD,0x46}; // Dudu
-	esp_base_mac_addr_set(new_mac);
+	esp_base_mac_addr_set(new_mac); // Wifi_STA=mac  wifi_AP=mac+1  BT=mac+2
+	WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));   // subnet FF FF FF 00
+
 	// Ethernet.begin(new_mac);
 	previousMillis = millis();
+
 	// event WiFi
 	WiFi.onEvent(WiFiEvent);
-	// Led
+
+	// Set pin mode
 	pinMode(EspLedBlue, OUTPUT);
+
 	// Get ssid pwd into flash ram
 	preferences.begin("wifi", false);
 	wifiSSID =  preferences.getString("ssid", "none");       // NVS key ssid
 	wifiPAWD =  preferences.getString("password", "none");   // NVS key password
 	preferences.end();
+
 	// Start AP and / or  STA
 	if (wifiSSID.indexOf("none")!=0) {
-		connectToHost();  // Try to connect to wifiSSID in NVM stored
+	  connectToHost();  // Try to connect to wifiSSID in NVM stored
 	} else {
-		if (wifi_connected==false)
-		setAccessPoint(); // Start accesPoint because wifiSSID host is not availabale
+		if (wifi_stationok==false)
+		  setAccessPoint(); // Start accesPoint because wifiSSID host is not availabale
 	}
+
 	// Mac address
-	Serial.print("Addr macAP : "); Serial.println( WiFi.softAPmacAddress().c_str() ); // Show mac address
-	Serial.print("Addr macSTA: "); Serial.println( WiFi.macAddress().c_str() ); // Show mac address
+	if (DBX_MSG)  {
+	  Serial.print("Addr macAP : "); Serial.println( WiFi.softAPmacAddress().c_str() ); // Show mac address
+	  Serial.print("Addr macSTA: "); Serial.println( WiFi.macAddress().c_str() ); // Show mac address
+  }
 }
 
 void loop() {
-	// Main loop from http
+
+	// Main loop from http decoded function
 	loopWifi();
+
 	// Main loop every new second elapes
-	if ( millis() - previousMillis > 1000L) {
-		// Time only when connected
-		if (wifi_connected) {
-			if (!getLocalTime(&timeinfo))
-			Serial.println("Failed to obtain time");
-			Serial.print("\r");Serial.print(getDate());
+	if ( millis() - previousMillis > 999L) {
+
+		// Get time
+		previousMillis = millis();
+
+		// Station is OK
+		if (wifi_stationok) {
+
+      // Close Access Point.
+			if ( wifi_accessPts ) {
+          if (DBX_MSG) Serial.println("Disconnect Access Point.");
+					WiFi.softAPdisconnect(true);
+					wifi_accessPts = false;
+			}
+
+			// Debug message for test
+			if (DBX_MSG) {
+			  if (!getLocalTime(&timeinfo))
+				  Serial.println("Failed to obtain time");
+			  else
+				  Serial.print("\r");Serial.print(getDate());
+			}
 		}
-		// Button Reset pressed more than 3 sec.
+
+		// Button BOOT is pressed more than 3 sec.
 		int btBoot = digitalRead(ButtonBoot);
 		if (btBoot==LOW) {
 			incBtBoot++;
-			Serial.println("btBoot pressed.");
+			if (DBX_MSG) Serial.println("btBoot pressed.");
 			if (incBtBoot>3) {
 				putPreferences("none","none");
 				setAccessPoint();
@@ -369,24 +415,9 @@ void loop() {
 		} else {
 			incBtBoot = 0;
 		}
-		// Get time
-		previousMillis = millis();
-		// Close AccessPoint if necessary
-		if ( closeAPinSec > 0)
-		closeAPinSec --;
-		if (closeAPinSec==1) WiFi.softAPdisconnect(true);
-		// Flash LED process
-		if (flashLed > 0) {
-			if ( (seconds & 0x1) == 0 ) {
-				digitalWrite(EspLedBlue, HIGH);
-			} 	else {
-				digitalWrite(EspLedBlue, LOW);
-				flashLed--;
-			}
-		}
-		// Timer incrementation
-		if (seconds++ == 59) {
+
+		// Timer
+		if (seconds++ == 59)
 			seconds = 0;
-		}
 	}
 }
